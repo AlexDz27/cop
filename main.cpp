e4ee16ff438fca63e6ca0fc4046a5e29b1d1b050
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-08-14 01:42:23 +0300

    w

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <string>
 using namespace std;
 
-int main() {
-  cout << "hello from cop!" << endl;
+int main(int argc, char* argv[]) {
+  cout << argc << endl;
+  cout << argv << endl;
+
+  cout << *argv << endl;
+
+  cout << *argv[0] << endl;
+  cout << *argv[1] << endl;  // TODO: here's the problem - i lack understanding
 }
